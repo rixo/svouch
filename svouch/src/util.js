cce84f12ac0ba18c8cfd9e32ed9ac4aa148e42f3
rixo <rixo@rixo.fr>
2020-12-06 15:30:34 +0100

    prettier

--- svouch/src/util.js
+++ svouch/src/util.js
@@ -1,6 +1,6 @@
-export const identity = x => x
+export const identity = (x) => x
 
-export const minZero = x => Math.max(0, x)
+export const minZero = (x) => Math.max(0, x)
 
 export const noop = () => {}
 
@@ -8,5 +8,5 @@ export const call = (x, f) => f(x)
 
 export const pipe = (...fns) => {
   fns = fns.filter(Boolean)
-  return x0 => fns.reduce(call, x0)
+  return (x0) => fns.reduce(call, x0)
 }
