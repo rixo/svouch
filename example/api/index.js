6513b6d27d36b3ae9f57a4cd71471c7caf8c9e63
rixo <rixo@rixo.fr>
2020-12-10 13:16:41 +0100

    remove vercel, add docker

--- example/api/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env node */
-
-const PouchDB = require('pouchdb')
-const express = require('express')
-const app = express()
-
-app.use('/db', require('express-pouchdb')(PouchDB))
-
-module.exports = app
